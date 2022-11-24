2c5e74b06a2fab4c70a40c9a8b5c9b1be6616227
sneakyzoul <christian-ccs@hotmail.com>
2022-11-24 14:23:44 +0100

    Done with plotting

--- HadlingData/GettingData.py
+++ HadlingData/GettingData.py
@@ -2,22 +2,18 @@ import numpy as np
 import pandas as pd
 import matplotlib as plt
 
-data = pd.read_csv('../Data/students.csv')
-ldata = data# .iloc[:0, :]
+data = pd.read_csv('../Data/TryData.csv')
 
-def fetchingData(datas):
-    datas = data[['first_name','ged']] #laver et nyt DataFrame ud fra de "navne" du gere vil have
+def fetchingData():
+    datas = pd.DataFrame(data) #laver et nyt DataFrame ud fra de "navne" du gere vil have
     df = datas.dropna()
-
-    print(df)
     return df
 
     
 
 
 if __name__ == '__main__':
-    print(ldata)
-    fetchingData(data.head())
+    print(fetchingData())
 
 
 
