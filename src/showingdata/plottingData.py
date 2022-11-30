0a75c3c849be25a8358b3da3b8c1f9f8c2e9d818
Snoopsie1 <78692409+Snoopsie1@users.noreply.github.com>
2022-11-30 15:10:35 +0100

    top8 plot done

--- src/showingdata/plottingData.py
+++ src/showingdata/plottingData.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from itertools import cycle, islice
 import handlingdata.GettingData as getd
 
 
@@ -61,9 +62,9 @@ def top3_plotting():
     second_place = data['secondPlaces'].tolist()
     third_place = data['thirdPlaces'].tolist()
 
-    plt.bar(x - 0.25, first_place, 0.2, color="#FFD700", edgecolor="#FFD700", label="Won", linewidth=5)
-    plt.bar(x, second_place, 0.2, color="#C0C0C0", edgecolor="#C0C0C0", label="Played", linewidth=5)
-    plt.bar(x + 0.25, third_place, 0.2, color="#CD7F32", edgecolor="#CD7F32", label="Lost", linewidth=5)
+    plt.bar(x - 0.25, first_place, 0.2, color="#FFD700", edgecolor="#FFD700", label="1st Place", linewidth=5)
+    plt.bar(x, second_place, 0.2, color="#C0C0C0", edgecolor="#C0C0C0", label="2nd Place", linewidth=5)
+    plt.bar(x + 0.25, third_place, 0.2, color="#CD7F32", edgecolor="#CD7F32", label="3rd Place", linewidth=5)
 
     plt.ylabel("Amount", fontsize=45)
     plt.xlabel("Character Name", fontsize=45)
@@ -78,5 +79,36 @@ def top3_plotting():
     plt.show()
 
 
+def top8_plotting():
+    data = getd.fetching_data()
+
+    x = np.arange(len(data['character'].tolist()))
+    names = data['character'].tolist()
+
+    # Make a list by cycling through the colors you care about
+    # to match the length of your data.
+    my_colors = list(islice(cycle(['#FFD700', '#C0C0C0', '#CD7F32', '#20715b',
+                                   '#20715b', '#20715b', '#20715b', '#20715b']), None, len(data)))
+
+    data.plot(kind="bar", color=my_colors, edgecolor="#000000")
+
+    plt.ylabel("Amount", fontsize=45)
+    plt.xlabel("Character Name", fontsize=45)
+    plt.title("Tournament Data", fontsize=45)
+    plt.xticks(x, names, rotation=45)
+    plt.yticks(fontsize=34)
+    plt.legend(['First Place', 'Second Place', 'Third Place', 'Fourth Place',
+                'Fifth Place', 'Sixth Place', 'Seventh Place', 'Eighth Place'], loc="upper right", fontsize=30)
+
+    plt.tick_params(axis='x')
+
+    current_fig = plt.gcf()
+    current_fig.set_size_inches(100, 20)
+    plt.tight_layout()
+
+    plt.show()
+
+
 if __name__ == '__main__':
     top3_plotting()
+    top8_plotting()
